c774c8512375588f7a9f93edcb1c90f5409c236f
Albert Chambers <albertchambers09@gmail.com>
2024-08-02 14:46:05 +0200

    add input and btn variables

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ Make it so that when you click the 'Add to cart' button, whatever is written in 
 */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-ad583-default-rtdb.europe-west1.firebasedatabase.app/"
+};
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
@@ -16,3 +20,18 @@ addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     console.log(inputValue)
 });
+
+
+
+let myItems = []
+const inputEl = document.getElementById("input-field")
+const inputBtn = document.getElementById("add-button")
+const cartList = document.getElementsById("cart-list")
+
+inputBtn.addEventListener("click", function() {
+    myItems.push(inputEl.value)
+})
+
+for (let i = 0; i < myItems.length; i++) {
+    cartList.innerHTML += "<li>" + myItems[i] + "</li>"
+}
