2720b5838ed2502c3181d078aae19bca1fe14d00
Albert Chambers <albertchambers09@gmail.com>
2024-08-02 13:41:44 +0200

    style body flex items

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+const firebaseConfig = {};
+
+const app = initializeApp(firebaseConfig);
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
     console.log(inputValue)
-})
+});
